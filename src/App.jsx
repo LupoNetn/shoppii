79c04800fac9b5b9ede3dedd9a492d0571b0fff4
oluwatobi <luponet07@gmail.com>
2025-09-15 12:12:02 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { createBrowserRouter, Route, RouterProvider } from "react-router";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/products",
+    element: <ProductsPage />,
+  },
+  {
+    path: "/products/:id",
+    element: <ProductDetails />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+]);
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <RouterProvider router={router} />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+
+const ProductsPage = () => {
+  return (
+    <div>
+      ProductsPage
+      <Outlet />
+    </div>
+  )
+}
+
+export default ProductsPage
